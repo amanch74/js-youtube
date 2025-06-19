c13948ee9e354e2bbdf551b03cc413371ff8dd1a
amanch74 <amanchy2002@gmail.com>
2025-06-19 06:31:16 +0000

    conversion

--- 01_basics/conversionoperations.js
+++ 01_basics/conversionoperations.js
@@ -16,13 +16,56 @@ undefined(undefined) => 0;
 
 let isLoggedIn = ""
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true ; 0 => false
 // ""(empty string) => false
 // "AMAN"(string) => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
 
-console.log(stringNumber)
+// console.log(stringNumber)
+
+/*-------------------------------Operations----------------------------------*/
+
+let value =3 
+let negval = -value
+
+// console.log(negval)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Aman"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+//Problems
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log("1" + 2 + 3)
+// console.log(1 + 2 + "4")
+
+// string phle hogi to saara string k hisaab se concatenate hoga wrna phle number hai to add hoke concatenate hoga
+
+// console.log(true) // => true
+// console.log(+true) // => 1
+// console.log(true+) // error
+console.log("")
+console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
+
+
